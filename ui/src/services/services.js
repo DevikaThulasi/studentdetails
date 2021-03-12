670ef47a1a12a920da4563eccf42120fa4759a78
Devika <devika.thulasi@mozanta.com>
2021-03-12 10:29:06 +0530

    added

---
+++ ui/src/services/services.js
@@ -0,0 +1,14 @@
+import http from "../http-common"
+class StudentService
+{
+getAll()
+{
+    return http.get("/students");
+   
+}
+create(data) {
+    return http.post("/students", data);
+  }
+
+}
+export default new StudentService();
